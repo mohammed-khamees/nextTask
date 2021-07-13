feb557a2b141e74c331c792aea1985cc5ae6b283
mohammed-khamees <mohammed.khamis1996@yahoo.com>
2021-07-13 16:05:03 +0300

    added the expo notifications

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import * as firebase from 'firebase';
+import * as Notifications from 'expo-notifications';
 
 // components
 import Home from './components/Home';
@@ -28,9 +29,33 @@ if (firebase.apps.length === 0) {
 	firebase.initializeApp(firebaseConfig);
 }
 
+// Notifications options
+Notifications.setNotificationHandler({
+	handleNotification: async () => ({
+		shouldShowAlert: true,
+		shouldPlaySound: true,
+		shouldSetBadge: false,
+	}),
+});
+
+async function schedulePushNotification() {
+	await Notifications.scheduleNotificationAsync({
+		content: {
+			title: "You've got mail! 📬",
+			body: 'Here is the notification body',
+			data: { data: 'Data goes here' },
+		},
+		trigger: { seconds: 2 },
+	});
+}
+
 export default function App() {
 	const [loaded, setLoaded] = useState(false);
 	const [loggedIn, setLoggedIn] = useState(false);
+
+	const [notification, setNotification] = useState(false);
+	const notificationListener = useRef();
+	const responseListener = useRef();
 
 	useEffect(() => {
 		firebase.auth().onAuthStateChanged((user) => {
@@ -42,6 +67,23 @@ export default function App() {
 				setLoggedIn(true);
 			}
 		});
+
+		notificationListener.current =
+			Notifications.addNotificationReceivedListener((notification) => {
+				setNotification(notification);
+			});
+
+		responseListener.current =
+			Notifications.addNotificationResponseReceivedListener((response) => {
+				console.log(response);
+			});
+
+		return () => {
+			Notifications.removeNotificationSubscription(
+				notificationListener.current,
+			);
+			Notifications.removeNotificationSubscription(responseListener.current);
+		};
 	}, []);
 
 	const logout = async () => {
@@ -77,8 +119,28 @@ export default function App() {
 
 	return (
 		<View style={styles.container}>
-			<Text>User is logged in</Text>
-			<Button title="logout" onPress={() => logout()} />
+			<View>
+				<Text>User is logged in</Text>
+				<Button title="logout" onPress={() => logout()} />
+			</View>
+
+			<View>
+				<View style={{ alignItems: 'center', justifyContent: 'center' }}>
+					<Text>
+						Title: {notification && notification.request.content.title}{' '}
+					</Text>
+					<Text>Body: {notification && notification.request.content.body}</Text>
+					<Text>
+						Data: {notification && notification.request.content.data.data}
+					</Text>
+				</View>
+				<Button
+					title="Press to schedule a notification"
+					onPress={async () => {
+						await schedulePushNotification();
+					}}
+				/>
+			</View>
 		</View>
 	);
 }
